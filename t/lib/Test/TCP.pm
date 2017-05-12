868370ade7beaa00876a6a5b750be496f4edf8ab
Sam Gillespie <samgwise@gmail.com>
2017-05-12 10:43:29 +1000

    Fixed up function signiture.
    Replaced VM types with Perl6 types.
    Added type for sleep.

--- t/lib/Test/TCP.pm
+++ t/lib/Test/TCP.pm
@@ -1,21 +1,19 @@
 use v6;
 unit class Test::TCP;
 
-sub wait_port(int $port, Str $host='127.0.0.1', :$sleep=0.1, int :$times=100) is export {
+sub wait_port(Int $port, Str $host='127.0.0.1', Numeric(Cool) :$sleep = 0.1, Int :$times=100) is export {
     LOOP: for 1..$times {
         try {
             my $sock = IO::Socket::INET.new(host => $host, port => $port);
             $sock.close;
 
-            CATCH { default {
+            CATCH {
                 sleep $sleep;
                 next LOOP;
-            } }
+            }
         }
         return;
     }
 
     die "$host:$port doesn't open in {$sleep*$times} sec.";
 }
-
-
